77529de80870a9b5b89e77e006afa1a34c3a3701
rey_han_so_hee <reyhanmauluddigame@gmail.com>
2026-05-14 14:43:09 +0700

    edit dataset churn

--- multipage_app/pages/2_⚠️​Churn.py
+++ multipage_app/pages/2_⚠️​Churn.py
@@ -65,13 +65,13 @@ st.sidebar.success('Select a page above')
 st.header('👤 Customer Churn Prediction')
 
 st.sidebar.divider()
-st.sidebar.header('User Input Features')
 
 current_dir = os.path.dirname(__file__)
 root_path = os.path.abspath(os.path.join(current_dir, ".."))
 path_ke_encoder = os.path.join(root_path, 'label_encoder.pkl')
 path_ke_model = os.path.join(root_path, 'model.pkl')
 path_ke_compare = os.path.join(root_path, 'best_model.csv')
+path_ke_customer = os.path.join(root_path, 'model.csv')
 
 try:
     encoders = joblib.load(path_ke_encoder)
@@ -81,17 +81,6 @@ except Exception as e:
     st.error(f"Gagal memuat file: {e}")
     st.info(f"Mencari di: {root_path}")
 
-list_gender = encoders['gender'].classes_
-gender_user = st.sidebar.selectbox("Gender", list_gender)
-gender_encoded = encoders['gender'].transform([gender_user])[0]
-
-list_country = encoders['country'].classes_
-country_user = st.sidebar.selectbox("Country", list_country)
-country_encoded = encoders['country'].transform([country_user])[0]
-
-list_traffic = encoders['traffic_source'].classes_
-traffic_user = st.sidebar.selectbox("Traffic Source", list_traffic)
-traffic_encoded = encoders['traffic_source'].transform([traffic_user])[0]
 
 
 # pembagian tab
@@ -100,6 +89,20 @@ tab1, tab2, tab3 = st.tabs(["🧠 Churn Model",
                       "🗂️ Raw Data & Filters"])
 
 with tab1:
+    st.sidebar.header('User Input Features')
+    list_gender = encoders['gender'].classes_
+    gender_user = st.sidebar.selectbox("Gender", list_gender)
+    gender_encoded = encoders['gender'].transform([gender_user])[0]
+
+    list_country = encoders['country'].classes_
+    country_user = st.sidebar.selectbox("Country", list_country)
+    country_encoded = encoders['country'].transform([country_user])[0]
+
+    list_traffic = encoders['traffic_source'].classes_
+    traffic_user = st.sidebar.selectbox("Traffic Source", list_traffic)
+    traffic_encoded = encoders['traffic_source'].transform([traffic_user])[0]
+
+
     st.write('Input customer details in the sidebar to predict churn risk.')
     age = st.sidebar.slider("Age", 10, 100, 30)
     total_order = st.sidebar.slider("Total Order", 1, 10, 5)
@@ -163,4 +166,69 @@ with tab2:
 
 
 with tab3:
-    st.write("tes")
+    st.subheader("Displaying Dataset of Customer Churn")
+    df_churn = pd.read_csv(path_ke_customer)
+    df_churn.index.name = 'no'
+    # df_churn = df_churn.reset_index()
+    ui_cols = st.columns(3)
+    df_churn_filter = df_churn.copy()
+    for i, col_name in enumerate(df_churn.select_dtypes(include=['number']).columns):
+        ui_idx = i % 3
+        with ui_cols[ui_idx]:
+            # Cek apakah kolom adalah y_prob atau bertipe float
+            if col_name == 'y_prob' or df_churn[col_name].dtype == 'float64':
+                min_val = float(df_churn[col_name].min())
+                max_val = float(df_churn[col_name].max())
+                step = 0.01 # Agar slider bisa digeser per 0.01
+                format = "%.2f"
+            else:
+                min_val = int(df_churn[col_name].min())
+                max_val = int(df_churn[col_name].max())
+                step = 1
+                format = "%d"
+            
+            if min_val < max_val:
+                selected_range = st.slider(
+                    f"Filter {col_name}", 
+                    min_val, 
+                    max_val, 
+                    (min_val, max_val),
+                    step=step,
+                    format=format,
+                    key=f"slider_{col_name}"
+                )
+                
+                df_churn_filter = df_churn_filter[
+                    (df_churn_filter[col_name] >= selected_range[0]) &
+                    (df_churn_filter[col_name] <= selected_range[1])
+                ]
+            else:
+                st.write(f"Filter {col_name}")
+                st.caption(f"Value: {min_val}")
+
+    st.dataframe(df_churn_filter, use_container_width=True)
+    st.info(f"Showing {len(df_churn_filter)} out of {len(df_churn)} records after applying filters.")
+
+
+
+
+    # Menampilkan detail label encoding
+    encoder_show = joblib.load(path_ke_encoder)
+    # Memeriksa apakah ini adalah LabelEncoder (memiliki atribut classes_)
+    kolom_pilihan = st.selectbox("Choose a column to view label details:", list(encoder_show.keys()))
+
+    # Ambil objek encoder berdasarkan pilihan
+    target_encoder = encoder_show[kolom_pilihan]
+
+    # Bongkar classes_ untuk mendapatkan arti numeriknya
+    if hasattr(target_encoder, 'classes_'):
+        mapping_dict = {
+            "Numeric Value": range(len(target_encoder.classes_)),
+            "Original Label": target_encoder.classes_
+        }
+        df_mapping = pd.DataFrame(mapping_dict)
+        
+        st.write(f"These are the mappings for column **{kolom_pilihan}**:")
+        st.dataframe(df_mapping, use_container_width=True, hide_index=True)
+    else:
+        st.warning("This object does not have a 'classes_' attribute.")
